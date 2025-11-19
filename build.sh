cd8183930f1200641f4a7ebae97e90e5e4dfcf73
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2025-11-19 02:49:08 +0200

    m

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/bash
+
+gcc -o baeagn \
+    baeagn.c \
+    -lm \
+    -O4 \
+    -march=native \
+    -w \
+    -D_CHESS960=$_CHESS960 \
+    -D_NOEDIT=$_NOEDIT \
+    -D_GAME_ini || exit 3
