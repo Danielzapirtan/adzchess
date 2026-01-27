e16a2420b8a91dca580693f1aee983fad13b28ae
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-01-27 18:32:07 +0200

    m

--- compiler/bas.sh
+++ compiler/bas.sh
@@ -5,6 +5,7 @@ shift
 ARGS="$@"
 
 g++ -o compiler/bas2c compiler/bas2c.cpp -w -O4 -march=native
+cat $SOURCE.bas
 compiler/bas2c $SOURCE.bas $SOURCE.c
 gcc -o $SOURCE $SOURCE.c -w -O4 -march=native
 $SOURCE $ARGS | sha256sum | tee cgb24.txt
