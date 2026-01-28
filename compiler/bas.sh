1802d167be72ffe5c8a7fcd57572df5bd7b42ead
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-01-28 08:00:29 +0200

    try_1024_1280

--- compiler/bas.sh
+++ compiler/bas.sh
@@ -8,4 +8,4 @@ g++ -o compiler/bas64toc compiler/bas64toc.cpp -w -O4 -march=native
 cat $SOURCE.bas
 compiler/bas64toc $SOURCE.bas $SOURCE.c
 gcc -o $SOURCE $SOURCE.c -w -O4 -march=native
-$SOURCE $ARGS | sha256sum | tee 1792_2048.txt
+$SOURCE $ARGS | sha256sum | tee 1024_1280.txt
