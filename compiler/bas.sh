480d04423de1d4f5b06a92493f30db9720019212
Danielzapirtan <antoniudanielzapirtan@gmail.com>
2026-01-28 12:04:06 +0200

    Sect12

--- compiler/bas.sh
+++ compiler/bas.sh
@@ -5,7 +5,7 @@ shift
 ARGS="$@"
 
 g++ -o compiler/bas64toc compiler/bas64toc.cpp -w -O4 -march=native
-cat $SOURCE.bas | sed -e "s/sect/11/g" >${SOURCE}_pp.bas
+cat $SOURCE.bas | sed -e "s/sect/12/g" >${SOURCE}_pp.bas
 cat $SOURCE_pp.bas
 compiler/bas64toc $SOURCE_pp.bas $SOURCE.c
 gcc -o $SOURCE $SOURCE.c -w -O4 -march=native
